edce99f4f13103a63762bb0a3ef6e075a6099d1b
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-10-30 22:16:44 +0000

    attack table

--- src/main.pl
+++ src/main.pl
@@ -53,3 +53,36 @@ convert_offset_to_axial(Q,R, AxialQ, AxialR) :- AxialR is -R+3,
 % +AxialQ, +AxialR, Q, R
 convert_axial_to_offset(AxialQ, AxialR, Q, R) :-    R is -AxialR+3,
                                                     Q is AxialQ + (AxialR - (AxialR /\ 1)) // 2.
+
+
+% according to the attack table, described in the report:
+
+% s1 - attacker can attack and not be eaten = 1
+% s2 - attacker can attack but is also eaten = 0
+% s3 - attacker cannot attack = -1
+
+% +attackerValue, +defenderValue
+
+% circle attack
+attack_checker(1,1) :- 1.
+attack_checker(1,3) :- 1.
+attack_checker(1,4) :- 1.
+attack_checker(1,5) :- 1.
+
+% triangle attack
+attack_checker(3,1) :- 0.
+attack_checker(3,3) :- 1.
+attack_checker(3,4) :- 1.
+attack_checker(3,5) :- 1.
+
+% square attack
+attack_checker(4,1) :- -1.
+attack_checker(4,3) :- 0.
+attack_checker(4,4) :- 1.
+attack_checker(4,5) :- 1.
+
+% pentagon attack
+attack_checker(5,1) :- -1.
+attack_checker(5,3) :- -1.
+attack_checker(5,4) :- -1.
+attack_checker(5,5) :- 1.
