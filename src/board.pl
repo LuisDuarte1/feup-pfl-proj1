2f15d8992f91a753b94ffd1435458ad14403106f
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-10-31 12:14:46 +0000

    middle board

---
+++ src/board.pl
@@ -0,0 +1,25 @@
+% A coordinates
+coord_to_axial_board(a0, 0,0).
+coord_to_axial_board(a1, 0,1).
+coord_to_axial_board(a2, 0,2).
+
+% B coordinates
+coord_to_axial_board(b0, 1,-1).
+coord_to_axial_board(b1, 1,0).
+coord_to_axial_board(b2, 1,1).
+coord_to_axial_board(b3, 1,2).
+
+% C coordinates
+coord_to_axial_board(c0, 2,-2).
+coord_to_axial_board(c1, 2,-1).
+coord_to_axial_board(c2, 2,1).
+coord_to_axial_board(c3, 2,2).
+
+% D coordinates
+coord_to_axial_board(d0, 2,-2).
+coord_to_axial_board(d1, 2,-1).
+coord_to_axial_board(d2, 2,1).
+coord_to_axial_board(d3, 2,2).
+
+
+  
