d94614ade8b35a44298ea785fb7b797eb079be7e
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-11-01 19:18:31 +0000

    bfs implemented dont know if it works

--- src/bfs.pl
+++ src/bfs.pl
@@ -12,4 +12,24 @@ get_adjacent(Board, Q1, R1, AdjacentList) :-
     maplist(call(calculate_adjacent,(Q1,R1)), Offsets, IntermediateList),
     include(call(check_tile_does_exist, Board), IntermediateList, AdjacentList).
 
+%in axial form
+filter_adjacent(Board, (Q2,R2), (Q2,R2)).
+filter_adjacent(Board, (Q2,R2), (Qi,Ri)) :- get_board_piece_axial(Board, Qi, Ri, Piece), Piece = 0. 
 
+%in axial form
+check_path_possible(Board, (Q1,R1), (Q2,R2)) :-
+    distance_axial(Board, Q1, R1, Q2, R2, Distance),
+    check_path_possible_bfs(Board, (Q2,R2), Distance, [(Q1,R1)]).
+
+
+%in axial form
+check_path_possible_bfs(_, (Q2,R2), _, _, [(Q2,R2) | _]) :- !.
+check_path_possible_bfs(_, (Q2,R2), 0, _, [(Q1,R1) | _]) :- fail.
+check_path_possible_bfs(Board, (Q2,R2),Distance, Visited, [(Q1,R1) | RestQueue]) :-
+    not(memberchk((Q1,R1), Visited)),
+    get_adjacent(Board, Q1,R1, AdjacentList),
+    NewDistance is Distance-1,
+    append([(Q1,R1)], Visited, NewVisited),
+    include(call(filter_adjacent, Board, (Q2,R2)), AdjacentList, TailQueue),
+    append(RestQueue, TailQueue, NewQueue),
+    check_path_possible_bfs(Board, (Q2,R2), NewDistance, NewVisited, NewQueue).
